1edb0b7d0856b235d2a013467b5572a3a7a64157
Nelson Jesus <n.jesus@tracknco.com>
2026-04-27 17:57:16 +0200

    Register custom user model in admin interface for enhanced user management

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
 
 # Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):  # permet d'adapter l'interface admin (backoffice) pour la gestion utilisateur (gestion des roles par exemple si on en avait besoin)
+    pass
