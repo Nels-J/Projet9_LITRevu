5eeed26c56684a5a6eb88d5c41e0350545b9e20e
Nelson Jesus <n.jesus@tracknco.com>
2026-04-27 19:16:26 +0200

    Refactor views.py: simplify imports, update success_url, and clean up FluxView docstring

--- users/views.py
+++ users/views.py
@@ -1,13 +1,12 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse_lazy
-
-from django.contrib.auth.views import(
+from django.contrib.auth.views import (
     LoginView as DjangoLoginView,
     LogoutView as DjangoLogoutView,
 )
-from django.views.generic import TemplateView, FormView, CreateView
-from django.conf import settings
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
 
 from users.forms import UserCreateForm
 
@@ -25,23 +24,26 @@ class LogoutView(DjangoLogoutView):
 class RegisterView(CreateView):
     template_name = 'users/register.html'
     form_class = UserCreateForm
-    success_url = reverse_lazy(settings.LOGIN_URL)
+    success_url = reverse_lazy('index')
 
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
 
 
-class FluxView(LoginRequiredMixin,TemplateView):
+class FluxView(LoginRequiredMixin, TemplateView):
+    ''' Affiche les posts des utilisateurs à l'utilisateur connecté.
+        LoginRequiredMixin : Assure que l'utilisateur est connecté pour accéder à la vue définie par template_name.
+        Sinon, redirection vers la page de connexion à l'aide de la constante LOGIN_URL défini dans settings.py
+        TemplateView : Affiche un template HTML.
+        template_name : Spécifie le template à utiliser pour afficher la vue.
+    '''
     template_name = 'users/flux.html'
-    login_url = settings.LOGIN_URL
 
 
 class PostsView(LoginRequiredMixin, TemplateView):
     template_name = 'users/posts.html'
-    login_url = settings.LOGIN_URL
 
 
 class AbonnementsView(LoginRequiredMixin, TemplateView):
     template_name = 'users/abonnements.html'
-    login_url = settings.LOGIN_URL
