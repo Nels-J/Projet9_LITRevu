ec68b0b7ae4b6e1f3883904104a361f2c796239e
Nelson Jesus <n.jesus@tracknco.com>
2026-04-28 11:44:03 +0200

    Refactor base.html, login.html, settings.py, and style.css: enhance structure, add static files support, and improve styling for authentication pages

--- LITRevu/settings.py
+++ LITRevu/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
 ]
 
 # Authentication settings
-AUTH_USER_MODEL = "users.User"
+AUTH_USER_MODEL = 'users.User'
 LOGIN_URL = 'login'  # Page par défaut si non connecté
 LOGIN_REDIRECT_URL = 'flux'  # Redirection après connexion
 LOGOUT_REDIRECT_URL = 'login'  # Redirection après déconnexion
@@ -117,5 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 APPEND_SLASH = True
